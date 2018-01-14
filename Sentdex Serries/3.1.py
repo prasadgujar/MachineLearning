f9e92bd9e43b520fb17afcff868d612ae60a0056
prasad <prasadgujar16@gmail.com>
2018-01-14 18:35:06 +0530

    files added

---
+++ Sentdex Serries/3.1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import quandl
+import math
+
+df = quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['lable'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.tail())
---
+++ ml library implementation/gbnb.py
@@ -0,0 +1,8 @@
+import numpy as np
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(X, Y)
+GaussianNB()
+print (clf.predict([[-0.8, -1]]))
